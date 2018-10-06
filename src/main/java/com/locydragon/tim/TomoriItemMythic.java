968fe7cd8f50fd18cba27de60ca3aa88572b4eb7
LocyDragon <2424441676@qq.com>
2018-10-06 13:40:45 +0800

    init

--- src/main/java/com/locydragon/tim/TomoriItemMythic.java
+++ src/main/java/com/locydragon/tim/TomoriItemMythic.java
@@ -2,10 +2,15 @@ package com.locydragon.tim;
 
 import com.locydragon.tim.commands.CommandBus;
 import com.locydragon.tim.io.FileConstantURLs;
+import com.locydragon.tim.io.FileContains;
 import com.locydragon.tim.io.listener.IOItemListener;
 import org.bukkit.Bukkit;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * @author LocyDragon
@@ -23,6 +28,7 @@ public class TomoriItemMythic extends JavaPlugin {
 		FileConstantURLs.init();
 		Bukkit.getPluginCommand(PLUGIN_CMD).setExecutor(new CommandBus());
 		registerEvents();
+		loadDefaultModel();
 	}
 
 	public void infoTask() {
@@ -43,5 +49,23 @@ public class TomoriItemMythic extends JavaPlugin {
 
 	public void registerEvents() {
 		Bukkit.getPluginManager().registerEvents(new IOItemListener(), this);
+	}
+
+	public void loadDefaultModel() {
+		File defaultFile = new File(FileConstantURLs.MODEL_LOCATION+"Default//Model.tim");
+		if (!defaultFile.exists()) {
+			boolean isSuccess = defaultFile.getParentFile().mkdirs();
+			try {
+				isSuccess = defaultFile.createNewFile() && isSuccess;
+				FileWriter writer = new FileWriter(defaultFile);
+				writer.write(FileContains.DEFAULT_MODEL_CONTENT);
+				writer.close();
+				if (isSuccess) {
+					Bukkit.getLogger().info("加载默认模板成功...");
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 }
---
+++ src/main/java/com/locydragon/tim/io/FileContains.java
@@ -0,0 +1,12 @@
+package com.locydragon.tim.io;
+
+public class FileContains {
+	public static String DEFAULT_MODEL_CONTENT = "ModelLore:\n" +
+			"  - \"&a&l&m一&b&l&m一&c&l&m一&e&l&m一&f&l&m一&1&l&m一&2&l&m一&3&l&m一\"\n" +
+			"  - \"&b加成攻击 >> &e<input> &b点 <<\"\n" +
+			"  - \"&b致命打击 >> &e<input> &b点 <<\"\n" +
+			"  - \"&a&l&m一&b&l&m一&c&l&m一&e&l&m一&f&l&m一&1&l&m一&2&l&m一&3&l&m一\"\n" +
+			"UsingMessage:\n" +
+			"  - \"&7[&a模板&7]&a欢迎使用我们的模板,现在,请在聊天栏输入武器加成的伤害点数.(阿拉伯数字一位)\"\n" +
+			"  - \"&7[&a模板&7]&a现在,请在聊天栏输入武器加成的致盲效果点数.(阿拉伯数字一位)\"";
+}
